92a3cab615b34746b6372b4f18f28b97be1e1db3
Musaab Abdalla <jamboosan209@gmail.com>
2023-12-14 23:11:43 +0100

    a user can add a post

--- src/js/feed.mjs
+++ src/js/feed.mjs
@@ -1,8 +1,14 @@
 
-import { postsUrl } from "./utils/variables.mjs";
+import { postsUrl, createPostUrl} from "./utils/variables.mjs";
 import renderAllPosts from "./ui/renderAllPosts.mjs"
-const feeds = document.querySelector("#feeds")
 
+const feeds = document.querySelector("#feeds")
+const addPostForm = document.querySelector("#add-new-post-form")
+const tagsInput = document.querySelector("#tags")
+const clearTags = document.querySelector("#clear-tags")
+const tagsView = document.querySelector("#tags-view")
+const submitPost = document.querySelector("#submit-post")
+const token = localStorage.getItem("token")
 
 
 
@@ -10,7 +16,7 @@ const feeds = document.querySelector("#feeds")
 
 async function getAllPosts(url){
     try {
-        const token = localStorage.getItem("token")
+        // const token = localStorage.getItem("token")
         const data = {
             method: "GET",
             headers:{
@@ -19,22 +25,76 @@ async function getAllPosts(url){
         }
         const response = await fetch(url,data)
         console.log(response)
-        const json = await response.json()
-        console.log(json)
+        const posts = await response.json()
+        console.log(posts)
         feeds.innerHTML = ""
-        renderAllPosts(json)
-
-        // json.forEach((post)=>{
-        //     feeds.innerHTML += `
-        //     <h2>${post.title}</h2>
-        //     `
-        // })
-            
-        
-
+        renderAllPosts(posts)
         
     } catch (error) {
         console.log(error)
     }
 }
-getAllPosts(postsUrl)
+// getAllPosts(postsUrl)
+
+
+
+//This part will add a tag to an array æwhen a user press Enter key when the value is not empty string.
+let tagsArray = []
+tagsInput.addEventListener("keydown",(e)=>{
+    if(e.key==="Enter"){
+        if(tagsInput.value){
+        // console.log(tagsInput.value)
+        tagsArray.push(tagsInput.value)
+        tagsView.value += `#${tagsInput.value} `
+        tagsInput.value=""
+    }
+}
+})
+//this button will empty the tags array and clear the value of tags text input
+clearTags.addEventListener("click",()=>{
+    tagsArray =[]
+    tagsInput.value=""
+    tagsView.value =""
+})
+console.log(submitPost)
+
+// add new post to the api
+submitPost.addEventListener("click",(e)=>{
+    e.preventDefault()
+    
+    const title = addPostForm.title.value
+    const imageUrl = addPostForm.imageUrl.value
+    const body = addPostForm.body.value
+    const tags = tagsArray
+    // console.log(title, imageUrl, body, tags)
+    const data ={
+        title:title,
+        body:body,
+        tags: tags,
+        media:imageUrl
+    }
+    addNewPost(createPostUrl,data)
+
+})
+//this function will 
+async function addNewPost(url,data){
+    try {
+        const response = await fetch(url,{
+            method:"POST",
+            headers:{
+                "Content-type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+    });
+    console.log(response)
+    const json = await response.json();
+    console.log(json)
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+
+
+}
--- src/js/utils/variables.mjs
+++ src/js/utils/variables.mjs
@@ -2,6 +2,8 @@ const API_BASE_URL = "https://api.noroff.dev/api/v1"
 const loginEndpoint = "/social/auth/login"
 const registerEndpoint ="/social/auth/register"
 const allPostsEndpoint = "/social/posts?_author=true&_comments=true&_reaction=true"
+const posts = "/social/posts"
 export const loginUrl = `${API_BASE_URL}${loginEndpoint}`
 export const registerUrl =`${API_BASE_URL}${registerEndpoint}`
 export const postsUrl = `${API_BASE_URL}${allPostsEndpoint}`
+export const createPostUrl = `${API_BASE_URL}${posts}`
